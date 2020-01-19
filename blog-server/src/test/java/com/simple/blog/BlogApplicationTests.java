cd4afa358895ecc8c3385541bdd201ca8ca84551
songning <songning@kedacom.com>
2020-01-19 16:48:13 +0800

    删除jpql-server模块

--- blog-server/src/test/java/com/simple/blog/BlogApplicationTests.java
+++ blog-server/src/test/java/com/simple/blog/BlogApplicationTests.java
@@ -1,8 +1,7 @@
 package com.simple.blog;
 
-
-import com.sn.jpql.JpqlParser;
-import com.sn.jpql.ParserParameter;
+import com.sleepy.jpql.JpqlParser;
+import com.sleepy.jpql.ParserParameter;
 import lombok.extern.slf4j.Slf4j;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
@@ -13,7 +12,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.persistence.EntityManagerFactory;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -30,11 +28,13 @@ public class BlogApplicationTests {
         return entityManagerFactory.unwrap(SessionFactory.class).openSession();
     }
 
+    /**
+     * 测试jpql是否可用
+     */
     @Test
-    public void test1() {
-        Map<String, Object> parameters = new HashMap<>(4);
-        parameters.put("tagNames", Arrays.asList("平台", "后台"));
-        String sql = jpqlParser.parse(new ParserParameter("testJpql.customSQL", parameters, "mysql")).getExecutableSql();
+    public void testJpql() {
+        Map<String, Object> params = new HashMap<>(4);
+        String sql = jpqlParser.parse(new ParserParameter("testJpql.customSQL", params, "mysql")).getExecutableSql();
         System.out.println();
     }
 }
